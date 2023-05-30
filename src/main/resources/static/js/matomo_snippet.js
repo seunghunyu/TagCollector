507c0bae019925d0ed828ea303b8821579882a3e
DESKTOP-F12VINP\User <yush@obzen.com>
2023-05-30 16:46:27 +0900

    conf 리소스 추가

--- src/main/resources/static/js/matomo_snippet.js
+++ src/main/resources/static/js/matomo_snippet.js
@@ -1,4 +1,5 @@
 var _mtm = window._mtm = window._mtm || [];
 _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-g.async=true; g.src='http://192.168.20.64/piwik/js/container_clKrVEUb.js'; s.parentNode.insertBefore(g,s);
+g.async=true; g.src='http://192.168.20.69:81/piwik/js/container_clKrVEUb.js'; s.parentNode.insertBefore(g,s);
+
